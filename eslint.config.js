537d2256442a07dc68be40609f29e2388caa2bc0
vanyauhalin <ivan.uhalin@onlyoffice.com>
2025-05-05 11:08:52 +0400

    update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -18,44 +18,12 @@
 
 import config from "@vanyauhalin/eslint-config"
 
-for (let c of config) {
-	if (!c.rules) {
-		continue
-	}
-
-	if ("no-inline-comments" in c.rules) {
-		c.rules["no-inline-comments"] = "off"
-	}
-
-	if ("jsdoc/tag-lines" in c.rules) {
-		c.rules["jsdoc/tag-lines"] = "off"
-	}
-
-	if ("n/hashbang" in c.rules) {
-		c.rules["n/hashbang"] = "off"
-	}
-
-	if ("tsdoc/syntax" in c.rules) {
-		c.rules["tsdoc/syntax"] = "off"
-	}
-
-	if ("typescript/ban-ts-comment" in c.rules) {
-		c.rules["typescript/ban-ts-comment"] = "off"
-	}
-
-	if ("typescript/prefer-nullish-coalescing" in c.rules) {
-		c.rules["typescript/prefer-nullish-coalescing"] = "off"
-	}
-
-	if ("typescript/switch-exhaustiveness-check" in c.rules) {
-		c.rules["typescript/switch-exhaustiveness-check"] = "off"
-	}
-
-	if ("unicorn/no-useless-undefined" in c.rules) {
-		c.rules["unicorn/no-useless-undefined"] = "off"
-	}
-}
-
 export default [
 	...config,
+	{
+		files: ["app/main.ts"],
+		rules: {
+			"n/hashbang": "off",
+		},
+	},
 ]
