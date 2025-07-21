fcd203ada86b2cd1a43ab7ad04602c60bd00b881
vanyauhalin <ivan.uhalin@onlyoffice.com>
2025-07-21 13:34:32 +0400

    use native basic auth

--- lib/client/internal/auth.ts
+++ lib/client/internal/auth.ts
@@ -1,60 +1,11 @@
-import {safeNew, safeSync} from "../../../util/result.ts"
-import type {Client} from "../../client.ts"
-import type {AuthenticateMeOptions} from "../auth.ts"
+import {safeSync} from "../../../util/result.ts"
 
 const headerApiKey = "Authorization"
 const headerAuthToken = "Authorization"
+const headerBasicAuth = "Authorization"
 const schemaApiKey = "Bearer"
+const schemaBasicAuth = "Basic"
 const cookieAuthToken = "asc_auth_key"
-const thresholdBasicAuth = 5 * 60 * 1000 // 5min
-
-export interface BasicAuthState {
-	token: string
-	expires: number
-}
-
-export async function checkBasicAuth(c: Client, s: BasicAuthState, o: AuthenticateMeOptions, input: Request): Promise<Error | undefined> {
-	if (s.expires > Date.now()) {
-		return
-	}
-
-	let a = await c.auth.authenticateMe(input.signal, o)
-	if (a.err) {
-		return new Error("Making authentication.", {cause: a.err})
-	}
-
-	let [d] = a.v
-
-	let errors: Error[] = []
-
-	let t = ""
-
-	if (d.token === undefined) {
-		errors.push(new Error("Token is not defined."))
-	} else {
-		t = d.token
-	}
-
-	let e = ""
-
-	if (d.expires === undefined) {
-		errors.push(new Error("Expiration date is not defined."))
-	} else {
-		e = d.expires
-	}
-
-	if (errors.length !== 0) {
-		return new Error("Checking authentication data.", {cause: errors})
-	}
-
-	let p = safeNew(Date, e)
-	if (p.err) {
-		return new Error("Parsing expiration date.", {cause: p.err})
-	}
-
-	s.token = t
-	s.expires = p.v.getTime() - thresholdBasicAuth
-}
 
 export function injectAuthKey(input: Request, k: string): Error | undefined {
 	let h = safeSync(input.headers.set.bind(input.headers), headerApiKey, `${schemaApiKey} ${k}`)
@@ -80,6 +31,15 @@ export function injectAuthToken(input: Request, t: string): Error | undefined {
 	}
 
 	h = safeSync(input.headers.set.bind(input.headers), "Cookie", c)
+	if (h.err) {
+		return new Error("Setting header.", {cause: h.err})
+	}
+}
+
+export function injectBasicAuth(input: Request, u: string, p: string): Error | undefined {
+	let v = Buffer.from(`${u}:${p}`, "utf8").toString("base64")
+
+	let h = safeSync(input.headers.set.bind(input.headers), headerBasicAuth, `${schemaBasicAuth} ${v}`)
 	if (h.err) {
 		return new Error("Setting header.", {cause: h.err})
 	}
